42700a1665f4f794a57ebdc499e7eadee03fa475
oddsummer56 <oddsummer56@gmail.com>
2024-10-22 11:30:45 +0900

    temurin

--- docker_restart.sh
+++ docker_restart.sh
@@ -7,7 +7,8 @@ echo "DOCKER RM"
 docker rm app
 
 echo "DOCKER RUN"
-docker run --name app -p 8080:8080 -d oddsummer/greeting:latest
+#docker run --name app -p 8080:8080 -d oddsummer/greeting:latest
+docker run --name app -p 8080:8080 -d oddsummer/temurin:latest
 
 
 echo "DONE"
